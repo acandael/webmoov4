dd80ca1c3c345b2325cc5406d8adeee35d93ad78
Anthony Candaele <info@anthonycandaele.com>
2025-04-29 10:24:44 +0200

    comfigure redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,62 @@ export default defineConfig({
 	},
 	output: "server",
 	adapter: vercel(),
+	redirects: {
+		"/en": {
+			status: 302,
+			destination: "/",
+		},
+		"/nl": {
+			status: 302,
+			destination: "/",
+		},
+		"/en/ecommerce-website": {
+			status: 302,
+			destination: "/ecommerce-website",
+		},
+		"/nl/standard-website//": {
+			status: 302,
+			destination: "/one-pager-website",
+		},
+		"/en/services/": {
+			status: 302,
+			destination: "/#producten",
+		},
+		"/nl/blog/": {
+			status: 302,
+			destination: "/blog",
+		},
+		"/en/premium-website/": {
+			status: 302,
+			destination: "/premium-website",
+		},
+		"/nl/services/": {
+			status: 302,
+			destination: "/#producten",
+		},
+		"/en/terms-of-service/": {
+			status: 302,
+			destination: "/algemene-voorwaarden",
+		},
+		"/nl/contact/": {
+			status: 302,
+			destination: "/contact",
+		},
+		"/nl/ecommerce-website/": {
+			status: 302,
+			destination: "/ecommerce-website",
+		},
+		"/nl/about/": {
+			status: 302,
+			destination: "/over-ons",
+		},
+		"/nl/premium-website/": {
+			status: 302,
+			destination: "/premium-website",
+		},
+		"/nl/terms-of-service/": {
+			status: 302,
+			destination: "/algemene-voorwaarden",
+		},
+	},
 });
